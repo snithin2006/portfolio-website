c395c602e6934642b17bccdbd2daa513425a508f
Nithin Sivakumar <nithin06.siva@gmail.com>
2025-01-02 00:35:53 +0530

    Banner and navbar update

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,7 +5,9 @@ const Banner = () => {
   return (
     <div className="banner-container">
       <div className="banner-dots"></div>
+      <div class="burst left">POW!</div>
       <h1 className="banner-text">Nithin Sivakumar</h1>
+      <div class="burst right">BAM!</div>
     </div>
   );
 };
