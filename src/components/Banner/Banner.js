218cf3c144ea18eb391f1d92a47296a3bd40965e
Nithin Sivakumar <nithin06.siva@gmail.com>
2026-04-07 12:50:00 -0500

    sizing changes

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,32 +1,13 @@
 
 import React from 'react';
 import './Banner.css';
-import TextBox from '../TextBox/Textbox';
 
 const Banner = () => {
   return (
     <div className="banner-container">
       <div className="banner-dots"></div>
 
-      <div className="burst left">
-        <img src="/images/bubbles/bubble1.png" alt="POW Bubble" />
-        <span className="bubble-text-left">SOFTWARE</span>
-      </div>
-
       <h1 className="banner-text">Nithin Sivakumar</h1>
-
-      <div className="burst right">
-        <img src="/images/bubbles/bubble3.png" alt="BAM Bubble" />
-        <span className="bubble-text-right">ENGINEER</span>
-      </div>
-
-      <div className="top-left-text">
-        <TextBox text="Hailing from..." fontSize='1.5vw' padding='0.2vw' backgroundColor={'#f9f401'} fontFamily={'CCMeanwhile'} />
-      </div>
-
-      <div className="bottom-right-text">
-        <TextBox text="Dallas, Texas" fontSize='1.5vw' padding='0.2vw' backgroundColor={'#f9f401'} fontFamily={'CCMeanwhile'} />
-      </div>
     </div>
   );
 };
